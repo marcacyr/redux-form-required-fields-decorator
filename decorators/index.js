a289676c133da14b3ce29172667b00d218bf4b72
Marc Cyr <marcacyr@gmail.com>
2018-05-18 10:51:14 -0700

    Add babelrc, the decorator itself, and various npm packages to handle the build

---
+++ decorators/index.js
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const { arrayOf, string } = PropTypes;
+
+const RequiredFields = requiredFields => ComposedComponent => (
+    class RequiredFields extends Component {
+
+        //--------------------------------------------------
+        // Context Methods
+        //--------------------------------------------------
+
+        getChildContext() {
+            return {
+                requiredFields
+            };
+        }
+
+        //--------------------------------------------------
+        // React LifeCycle Methods
+        //--------------------------------------------------
+
+        render() {
+            return <ComposedComponent { ...this.props } />
+        }
+    }
+);
+
+RequiredFields.displayName = 'RequiredFields';
+RequiredFields.childContextTypes = { requiredFields: arrayOf(string).isRequired };
+
+export default RequiredFields;
